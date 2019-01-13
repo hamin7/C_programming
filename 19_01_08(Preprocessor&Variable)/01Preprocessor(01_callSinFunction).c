1a1d9f59a6d0d599db0a30c06c9124e89f151f9d
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-13 23:18:20 +0900

    Update 01Preprocessor(01_callSinFunction).c

--- 19_01_08(Preprocessor&Variable)/01Preprocessor(01_callSinFunction).c
+++ 19_01_08(Preprocessor&Variable)/01Preprocessor(01_callSinFunction).c
@@ -7,7 +7,7 @@ int main()
 {
 	for (int degree = 0; degree <= 180; degree += 30)
 	{
-		double radian = M_PI * degree / 180;
+		double radian = PI * degree / 180;
 		double s = sin(radian);
 		printf("sin(%d도) = %lf \n", degree, s);
 	}
