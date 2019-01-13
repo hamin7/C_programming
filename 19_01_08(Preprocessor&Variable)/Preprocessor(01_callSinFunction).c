231f2abfaa5dc35c8ceb7fad3e091f0ab5cdd86d
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-13 23:14:26 +0900

    Update and rename Preprocessor(01_math.h).c to Preprocessor(01_callSinFunction).c

--- 19_01_08(Preprocessor&Variable)/Preprocessor(01_callSinFunction).c
+++ 19_01_08(Preprocessor&Variable)/Preprocessor(01_callSinFunction).c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#define _USE_MATH_DEFINDES
 #include <math.h>
 #define PI 3.141592
+#define _USE_MATH_DEFINES
 
-
-
-int main() {
+int main()
+{
 	for (int degree = 0; degree <= 180; degree += 30)
 	{
 		double radian = M_PI * degree / 180;
 		double s = sin(radian);
 		printf("sin(%d도) = %lf \n", degree, s);
 	}
-
-	return 0;
 }
+
+
