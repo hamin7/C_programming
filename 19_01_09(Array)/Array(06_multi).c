fe1b9eb638badd56ddc922f8128af7daa6630d85
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-13 23:59:58 +0900

    Update Array(06_multi).c

--- 19_01_09(Array)/Array(06_multi).c
+++ 19_01_09(Array)/Array(06_multi).c
@@ -6,7 +6,7 @@
 
 int main()
 {
-	int menu;
+	int menu = 1;
 
 	srand(time(NULL));
 	int A[2][2], B[2][2], C[2][2];
@@ -32,7 +32,11 @@ int main()
 		printf("원하는 연산은? \n1.덧셈\n2.뺄셈\n3.곱셈\n0.종료\n");
 		scanf("%d", &menu);
 		if (menu == 1) {
-
+			for (int i = 0; i < 2; i++) {
+				for (int j = 0; j < 2; j++) {
+					C[i][j] = A[i][j] + B[i][j];
+				}
+			}
 		}
 		else if (menu == 2) {
 
@@ -47,8 +51,12 @@ int main()
 		for (int i = 0; i < 2; i++) {
 			printf("C[%d   %d]\n", C[i][0], C[i][1]);
 		}
+		printf("\n");
 
 	} while (menu != 0);
 
 	return 0;
 }
+
+
+// 뺄셈이랑 곱셈은 노가다라서 구현 안함
