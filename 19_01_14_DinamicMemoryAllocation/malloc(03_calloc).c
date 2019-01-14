907b810a436ad0c7f2e6b6632d20e7d66c21dc7d
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-14 20:11:43 +0900

    Update malloc(03_calloc).c

--- 19_01_14_DinamicMemoryAllocation/malloc(03_calloc).c
+++ 19_01_14_DinamicMemoryAllocation/malloc(03_calloc).c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
+
 int main() {
-	int *p_student;
 	int n = 0;
 	int sum = 0;
-	printf("학생수는?");
+
+	printf("학생 수는?");
+
 	scanf("%d", &n);
 
 	int *score = (int *)calloc(n, sizeof(int));
@@ -14,17 +16,20 @@ int main() {
 
 	if (score != NULL) {
 		for (int i = 0; i < n; i++) {
-			printf("your score: ");
-			scanf("%d", &score[i]);  // score + i로 바뀜 컴파일 할때.
-			sum += score[i];     // sum += *(score + i);    같은 식이다.   // scanf("%d", p); sum += *p; p++; 포인터 자체 변화 노노 새로운 포인터를 지정.
+			printf("your score :");
+			scanf("%d", &score[i]);			// 컴파일 시 score + i로 바뀜
+			sum += score[i];			// sum += *(score+i); 와 같은 식이다.
+			//scanf("%d", p);
+			//sum += *p;
+			//p++;			// 포인터 자체 변화 노노 새로운 포인터를 지정.
 		}
-		printf("총점 : %d \n", sum);
-		free(score);
 	}
 	else {
-		printf("Memory allocation error!");
+		printf("Memory Allocation error! \n");
 		exit(1);
 	}
+	free(score);
+	printf("평균 점수는 : %lf", (double)sum / n);
 
 	return 0;
 }
