021f47393c8a6bd66d6fb1e22bf261ba304f31d1
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-14 20:33:20 +0900

    Update malloc(04).c

--- 19_01_14_DinamicMemoryAllocation/malloc(04).c
+++ 19_01_14_DinamicMemoryAllocation/malloc(04).c
@@ -1,35 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
+
 int main() {
-	int n = 3, i = 0;
+	int n = 3;
+	int i = 0;
 	int sum = 0;
 
 	int *score = (int *)calloc(n, sizeof(int));
+	int *p = score;
 
 	if (score != NULL) {
-		while (1) {
+		while (1)
+		{
 			int temp;
 			scanf("%d", &temp);
 			if (temp < 0) {
 				break;
 			}
-			if (i >= 0) {
-				int *score_new = (int *)calloc(++n, sizeof(int));
-				for (int j = 0; j < n - 1; j++) {
-					score_new[j] = score[j];
+			else if (temp >= 0){
+				int *new_score = (int *)calloc(++n, sizeof(int));
+				for (int j = 0; j <= n - 1; j++) {
+					*(new_score+j) = *(score + j);			// new_score[j] = score[j]
 				}
 				free(score);
-				score = score_new;
+				score = new_score;
 			}
-			score[i] = temp;
-			sum += score[i];
+			*(score + i) = temp;			// score[i] = temp;
+			sum += *(score + i);			// sum += score[i];
 			i++;
 		}
-		printf("----------\n");
-		printf("학생 수: %d\n", i);
-		printf("총점: %d\n", sum);
-		return 0;
+		free(score);
+		printf("학생수는 %d \n", i);
+		printf("평균 점수는 : %lf \n", (double)sum / (i));
 	}
+	else {
+		printf("Memory Allocation error! \n");
+		exit(1);
+	}
+	return 0;
 }
