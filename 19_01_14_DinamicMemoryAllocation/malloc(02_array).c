c91b926153701004afb3dbbb91ae873d46fd26b0
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-14 19:51:43 +0900

    Update malloc(02_array).c

--- 19_01_14_DinamicMemoryAllocation/malloc(02_array).c
+++ 19_01_14_DinamicMemoryAllocation/malloc(02_array).c
@@ -1,30 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
-int main(){
-	int *p_student;
+
+int main() {
 	int n = 0;
 	int sum = 0;
-	printf("학생수는?");
+
+	printf("학생 수는?");
 	scanf("%d", &n);
 
 	int score[100] = { 0 };
 
-	p_student = (int *)malloc(sizeof(int) * n);
 
 	for (int i = 0; i < n; i++) {
-		if (p_student != NULL) {
-			printf("your score: ");
-			scanf("%d", &score[i]);
-			sum += score[i];
-		}
-		else {
-			printf("Memory allocation error!");
-		}
-		printf("총점 : %d \n", sum);
+		scanf("%d", &score[i]);
+		sum += score[i];
 	}
-	free(p_student);
+
+	printf("평균 점수는 : %lf", (double)sum / n);
 
 	return 0;
 }
