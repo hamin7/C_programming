9583d58808ea7cb75be5af74f661b4f0750b969f
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-14 20:07:56 +0900

    Update malloc(02_malloc).c

--- 19_01_14_DinamicMemoryAllocation/malloc(02_malloc).c
+++ 19_01_14_DinamicMemoryAllocation/malloc(02_malloc).c
@@ -6,19 +6,22 @@
 int main() {
 	int n = 0;
 	int sum = 0;
-	;
 
 	printf("학생 수는?");
 
 	scanf("%d", &n);
 
 	int *score = (int *)malloc(sizeof(int)*n);
+	int *p = score;
 
 	if (score != NULL) {
 		for (int i = 0; i < n; i++) {
 			printf("your score :");
-			scanf("%d", &score[i]);
-			sum += score[i];
+			scanf("%d", &score[i]);			// 컴파일 시 score + i로 바뀜
+			sum += score[i];			// sum += *(score+i); 와 같은 식이다.
+			//scanf("%d", p);
+			//sum += *p;
+			//p++;			// 포인터 자체 변화 노노 새로운 포인터를 지정.
 		}
 	}
 	else {
