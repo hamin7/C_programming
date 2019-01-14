e91b5b4bbaf198542dc28339825f4faff5aad52d
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-14 22:31:31 +0900

    Create DoublePointer(00_value).c

---
+++ 19_01_14_DinamicMemoryAllocation/DoublePointer(00_value).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+
+int main() {
+	int i = 20;
+	int *pi = &i;			// 포인터 선언.
+	int **dpi = &pi;			// 이중 포인터 선언.
+
+	printf("%p %p %p\n", &i, pi, *dpi);			// 모두 변수 i의 주소값을 참조하는 방식.
+
+	*pi = i + 2;			// i = i + 2;
+	printf("%d %d %d\n", i, *pi, **dpi);
+
+	**dpi = *pi + 2;			// i = i + 2;
+	printf("%d %d %d\n", i, *pi, **dpi);
+
+	return 0;
+}
