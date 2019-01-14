b5e4dbf07d3b6bcb5b3bde81b60c97bf6da83666
hamin7 <33690039+hamin7@users.noreply.github.com>
2019-01-14 20:43:41 +0900

    Update malloc(04_memcpy).c

--- 19_01_14_DinamicMemoryAllocation/malloc(04_memcpy).c
+++ 19_01_14_DinamicMemoryAllocation/malloc(04_memcpy).c
@@ -1,37 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 #include <string.h>
+#include <limits.h>
+
 
 int main() {
-	int n = 3, i = 0;
+	int n = 3;
+	int i = 0;
 	int sum = 0;
 
 	int *score = (int *)calloc(n, sizeof(int));			// score라는 포인터가 calloc(n=3 * 4바이트)을 가리키도록 해줌.
+	int *p = score;
 
 	if (score != NULL) {
-		while (1) {
+		while (1)
+		{
 			int temp;
 			scanf("%d", &temp);			// 점수를 입력받음.
 			if (temp < 0) {
 				break;			// 입력값이 음수라면 종료.
 			}
-			if (i >= 0) {
-				int *score_new = (int *)calloc(++n, sizeof(int));			// score_new라는 동적 메모리 할당을 새로 만듬.
-				memcpy(score_new, score, sizeof(int) * (n - 1));		//  아래 세줄과 같은 기능을 함.
-				//for (int j = 0; j < n - 1; j++) {
-				//	score_new[j] = score[j];
-				//}
-				free(score);		// 원래 있던 score 메모리를 지움.
-				score = score_new;			// score 포인터가 이제는 score_new 메모리를 가르키도록.
+			if (i >= 0){				// 여기가 의문임 3명 이상일 때가 아니라 그냥 바로 늘려줌.
+				int *new_score = (int *)calloc(++n, sizeof(int));			// new_score라는 동적 메모리 할당을 새로 만듬.
+				memcpy(new_score, score, sizeof(int) * (n - 1));			// 새로운 new_score로 score를 복사해서 붙여넣음.
+				free(score);			// 원래있던 score 메모리를 지움.
+				score = new_score;			// score 포인터가 이제는 new_score 메모리를 가르키도록.
 			}
-			score[i] = temp;
-			sum += score[i];
+			*(score + i) = temp;			// score[i] = temp;
+			sum += *(score + i);			// sum += score[i];
 			i++;
 		}
-		printf("----------\n");
-		printf("학생 수: %d\n", i);
-		printf("총점: %d\n", sum);
-		return 0;
+		free(score);
+		printf("학생수는 %d \n", i);
+		printf("평균 점수는 : %lf \n", (double)sum / (i));
 	}
+	else {
+		printf("Memory Allocation error! \n");
+		exit(1);
+	}
+	return 0;
 }
+
+// 3명 이상일 때가 아니라 그냥 바로 늘려줌.
